6b70edfa95d25df68a61bf5bd5916602247b1e54
Arun AS <ognitiotechnologies@gmail.com>
2021-08-07 22:24:53 +0530

    custom hook taskObj undefined error

--- features/Index.jsx
+++ features/Index.jsx
@@ -2,39 +2,29 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { Container, Grid } from '@material-ui/core';
 import NewTask from './newTask';
 import Tasks from './tasks';
+import useHttp from '../hooks/use-http';
 
 const HomePage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://udemy-app-2dac9-default-rtdb.firebaseio.com/tasks.json'
-      );
+  const transferedTasks = (taskObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-      console.log(data);
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp({
+    url: 'https://udemy-app-2dac9-default-rtdb.firebaseio.com/tasks.json',
+    transferedTasks,
+  });
 
   useEffect(() => {
     fetchTasks();
